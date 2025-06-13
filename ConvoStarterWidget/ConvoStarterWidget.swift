7304cfdde1a7217a373aedfcfc037a5c6edf2530
Andre van Tonder <andre@drevan.me>
2025-06-13 10:17:17 +1000

    chore: Set up basic structure

---
+++ ConvoStarterWidget/ConvoStarterWidget.swift
@@ -0,0 +1,84 @@
+//
+//  ConvoStarterWidget.swift
+//  ConvoStarterWidget
+//
+//  Created by Andre van Tonder on 13/6/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+
+//    func relevances() async -> WidgetRelevances<Void> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct ConvoStarterWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct ConvoStarterWidget: Widget {
+    let kind: String = "ConvoStarterWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                ConvoStarterWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                ConvoStarterWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    ConvoStarterWidget()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ ConvoStarterWidget/ConvoStarterWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ConvoStarterWidgetBundle.swift
+//  ConvoStarterWidget
+//
+//  Created by Andre van Tonder on 13/6/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ConvoStarterWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ConvoStarterWidget()
+    }
+}
